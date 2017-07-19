a610280a63884905fd69b049110302198ce82fe0
vbbphc <f2016165@hyderabad.bits-pilani.ac.in>
2017-07-19 23:51:50 +0530

    Initial Commit

--- app/src/main/java/com/example/vaibhavballoli/usefullibrariesapp/MainActivity.java
+++ app/src/main/java/com/example/vaibhavballoli/usefullibrariesapp/MainActivity.java
@@ -10,16 +10,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 /*
-* TODO:1. RecyclerView with best practices[handling clicks, etc.].
-* TODO:2. Using ButterKnife
-* TODO:3. Picasso Image Loading(and Downloading) Library.
-* TODO:4. Gson Serializer to parse JSON objects.
-* TODO:5. Dart and Henson to handle intents by cutting down the classic(boring) code.
-* TODO:6. Try to do retrofit - GitHub API possibly - Easy Access and no API keys.
-* TODO:7. Activity regarding usage of camera - Google Vision API[Barcode and QRCode Scanning, Text Detection, Face Detection etc.] - Possibly Barcode and QRCode here.
-* TODO:8. A SignIn and SignUp page with Realm and encryption alongside.
-* TODO:9. Realm Example Codes - Basic Operations.
-* TODO:10. Some usage of fragments.
+* TODO:0. Adding appropriate links to videos and references to each library usage and try incorporating Shared Preferences and Settings to give a better UI control to the user. - TO BE DONE
+* TODO:1. RecyclerView - List with best practices[handling clicks, etc.]. - TO BE DONE
+* TODO:2. Using ButterKnife. - TO BE DONE
+* TODO:3. Picasso Image Loading(and Downloading) Library. - TO BE DONE
+* TODO:4. Gson Deserializer to parse JSON objects. - TO BE DONE
+* TODO:5. Dart and Henson to handle intents and intent extras by cutting down the classic(boring) code. - TO BE DONE
+* TODO:6. Try to do retrofit - GitHub API possibly - Easy Access and no API keys. - TO BE DONE
+* TODO:7. Activity regarding usage of camera - Google Vision API[Barcode and QRCode Scanning, Text Detection, Face Detection etc.] - Possibly Barcode and QRCode here. - TO BE DONE
+* TODO:8. A SignIn and SignUp page with Realm and encryption alongside. - TO BE DONE
+* TODO:9. Realm - Database Example Codes - Basic Operations. - TO BE DONE
+* TODO:10. Some usage of fragments. - TO BE DONE
+* TODO:11. Dependency Injections using Dagger2. - TO BE DONE
+* TODO.12. Follow Material Design as much as possible. - TO BE DONE
 *  */
 public class MainActivity extends AppCompatActivity {
 
@@ -29,15 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Here will be the code for a navigation drawer from the landing page after the login. Navigation Drawer consists of all activities and fragments to add.
+        //Flow of the App:
+        /*
+        * The main activity will be empty and just with navigation drawer.
+        * The navigation view will contain various activity links, grouped and with navigation header.
+        * */
     }
 
     @Override
@@ -52,12 +52,8 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //Lite
 
         return super.onOptionsItemSelected(item);
     }
