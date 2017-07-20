aa42d8a0ad4d4c1bcaa95420612895937f39cd3a
vbbphc <f2016165@hyderabad.bits-pilani.ac.in>
2017-07-20 17:58:57 +0530

    Realm Configuration Setup

--- app/src/main/java/com/example/vaibhavballoli/usefullibrariesapp/Realm/LibraryApp.java
+++ app/src/main/java/com/example/vaibhavballoli/usefullibrariesapp/Realm/LibraryApp.java
@@ -1,8 +1,29 @@
 package com.example.vaibhavballoli.usefullibrariesapp.Realm;
 
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by vaibhavballoli on 20/07/17.
  */
 
-public class LibraryApp {
+public class LibraryApp extends Application {
+    private static Realm realm;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().name("").schemaVersion(42).build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+        //Different realm configurations can be made depending upon the need.
+        //The manifest also must be changed and set to android:name = "LibraryApp"
+        realm = Realm.getDefaultInstance();
+
+    }
+
+    public static Realm getRealmInstance() {
+        return realm;
+    }
 }
